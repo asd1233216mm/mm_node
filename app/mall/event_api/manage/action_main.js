1284954ffb81d02e18c958286d781ea6ba4ca2a2
asd1233216mm <2502214601@qq.com>
2020-04-17 00:15:21 +0800

    更新商城，系统，用户

--- app/mall/event_api/manage/action_main.js
+++ app/mall/event_api/manage/action_main.js
@@ -1,6 +1,6 @@
-var api = $.api_admin('mall_manage', '商城管理端');
+var api_test = $.api_admin('mall_manage', '商城管理端');
 // 首次启动更新api接口;
-api.update('mall/');
+api_test.update('mall/');
 
 var sql = $.mysql_admin('sys', __dirname);
 sql.setConfig($.config.mysql);
@@ -14,6 +14,6 @@ sql.open();
  */
 async function main(ctx, db) {
 	$.push(db, sql.db(), true);
-	return api.run(ctx, db);
+	return api_test.run(ctx, db);
 };
 exports.main = main;
